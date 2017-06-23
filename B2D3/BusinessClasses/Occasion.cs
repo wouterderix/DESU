ab1d65882b592024b36b8a4f1024f9354e69b508
Jeroen-Laptop <Jeroen@JeroenOnline.biz>
2017-06-23 13:06:24 +0200

    Eerste opzet BusinessClasses en voorbeeld (1)

---
+++ B2D3/BusinessClasses/Occasion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace B2D3.Classes
+{
+    public partial class Occasion
+    {
+        public string test1()
+        {
+            return "test";
+        }
+
+
+
+        public List<Occasion> getOccasion()
+        {
+            //Query getOccasion
+            return occasionList;
+        }
+
+        public Occasion getOccasion(int id)
+        {
+            //Query getOcassion where id = id
+            //ocassion = blabla;
+            return occasion;
+        }
+
+        public void deleteOccasion()
+        {
+            //Delete occasion
+        }
+    }
+}
---
+++ B2D3/ControlClasses/Occasion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using B2D3.Classes;
+
+namespace B2D3.Classes.CC
+{
+    public class OccasionCC
+    {
+        Occasion o;
+        public void test()
+        {
+            o.test1();
+        }
+    }
+}
