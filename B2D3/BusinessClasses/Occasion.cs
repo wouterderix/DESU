3591c133a7bba649cc1f665cc1c65cb0c4977709
robkle <robinklein97@gmail.com>
2017-06-23 16:10:04 +0200

    querry reprobeert. krijg failed to emit module error

--- B2D3/BusinessClasses/Occasion.cs
+++ B2D3/BusinessClasses/Occasion.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using B2D3.Classes;
 
 
 namespace B2D3.Classes
@@ -15,18 +16,32 @@ namespace B2D3.Classes
 
 
 
-        public List<Occasion> getOccasion()
+        public List<Occasion> getMultipleOccasion(bool isDeleted)
         {
-            //Query getOccasion
+            List<Occasion> occasionList = null;
+
+            IEnumerable<Occasion> OccasionQuerry =
+                from Occasion in Classes
+                where IsDeleted = isDeleted
+                select Occasion;
+
+            
+            foreach (Occasion occasion in OccasionQuerry) {
+                occasionList.Add(occasion);
+            }
+
             return occasionList;
         }
 
-        public Occasion getOccasion(int id)
-        {
-            //Query getOcassion where id = id
-            //ocassion = blabla;
-            return occasion;
-        }
+        //public Occasion getOccasion(int id)
+        //{
+        //    IEnumerable<Occasion> OccasionQuerry =
+        //        from Occasion in Classes
+        //        where 
+        //        select Occasion;
+
+        //    return occasion;
+        //}
 
         public void deleteOccasion()
         {
