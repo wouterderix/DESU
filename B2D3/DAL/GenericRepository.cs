82d9a3115fd0931fb95b25c997182cdfa59fc847
Dennis <Dennis@LAPTOPDENNIS>
2017-06-25 05:13:23 +0200

    Implemented repositories. Ordened classes

---
+++ B2D3/DAL/GenericRepository.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using B2D3.DAL;
+
+namespace B2D3.DAL
+{
+    public abstract class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private bool _isDisposed;
+        protected DbContext Context
+        { get; private set; }
+        internal DbSet<T> DbSet;
+
+
+        public GenericRepository(DbContext context)
+        {
+            if (context == null)
+            { throw new NullReferenceException("DbContext may not be null!"); }
+            Context = context;
+
+            DbSet = Context.Set<T>();
+        }
+        ~GenericRepository()
+        { Context?.Dispose(); }
+
+        public virtual IEnumerable<T> Search(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>,
+            IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = "")
+        {
+            IQueryable<T> query = DbSet;
+
+            if (filter != null)
+            { query = query.Where(filter); }
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            { query = query.Include(includeProperty); }
+
+            if (orderBy != null)
+            { return orderBy(query).ToList(); }
+            else
+            { return query.ToList(); }
+        }
+        public virtual T Single(params object[] primaryKeys)
+        { return FindByID(primaryKeys); }
+        public virtual T SingleOrDefault(params object[] primaryKeys)
+        { return FindByID(primaryKeys); }
+        public virtual bool Exists(params object[] primaryKeys)
+        { return FindByID(primaryKeys) != null; }
+        public virtual T FindByID(params object[] primaryKeys)
+        { return DbSet.Find(primaryKeys); }
+        public virtual void Insert(T entity)
+        { DbSet.Add(entity); }
+        public virtual void Delete(params object[] primaryKeys)
+        {
+            Delete(FindByID(primaryKeys));
+        }
+        public virtual void Delete(T entityToDelete)
+        {
+            if (Context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                DbSet.Attach(entityToDelete);
+            }
+            DbSet.Remove(entityToDelete);
+        }
+        public virtual void Update(T entityToUpdate)
+        {
+            DbSet.Attach(entityToUpdate);
+            Context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+        public virtual IEnumerable<T> GetAll()
+        {
+            return DbSet.ToList();
+        }
+        public virtual void Persist()
+        { Context.SaveChanges(); }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                { Context.Dispose(); }
+            }
+            _isDisposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ B2D3/DAL/ProductRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using B2D3.Classes;
+using B2D3.GlobalClasses;
+
+namespace B2D3.DAL
+{
+    public sealed class ProductRepository : VersionRepository<Product>
+    {
+        public ProductRepository(DbContext context) : base(context)
+        {
+            
+        }
+
+        public IQueryable<Product> FilterProducts(Expression<Func<Product, bool>> filter)
+        {
+            //Retrieve all latest versions.
+            IQueryable<Product> latestProducts = GetAllLatest();
+
+            //Apply filter and return.
+            return filter != null ? latestProducts.Where(filter) : latestProducts;
+        }
+        public IQueryable<Product> FilterProducts(ProductQuerryModel searchInstructions)
+        {
+            IQueryable<Product> products = GetAllLatest();
+
+            //Filter ID
+            if (searchInstructions.ID.HasValue)
+            { products = products.Where(x => x.HistoryID == searchInstructions.ID); }
+
+            //Filter IsDeleted
+            if (searchInstructions.IsDeleted.HasValue)
+            { products = products.Where(x => x.IsDeleted == searchInstructions.IsDeleted); }
+
+            //Filter IsApproved
+            if (searchInstructions.IsApproved.HasValue)
+            { products = products.Where(x => x.IsApproved == searchInstructions.IsApproved); }
+
+            //Filter name
+            if (!string.IsNullOrWhiteSpace(searchInstructions.Name))
+            { products = products.Where(x => x.Name.ToLower().Contains(searchInstructions.Name.ToLower())); }
+
+            //Filter Min. price
+            if (searchInstructions.MinPrice.HasValue)
+            { products = products.Where(x => x.Price >= searchInstructions.MinPrice); }
+
+            //Filter Max. price
+            if (searchInstructions.MaxPrice.HasValue)
+            { products = products.Where(x => x.Price <= searchInstructions.MaxPrice); }
+
+            //Filter Category
+            if (searchInstructions.Category != null)
+            { products = products.Where(x => x.ProductCategory.Id == searchInstructions.Category.Id); }
+
+            //Filter OperationArea
+            if (searchInstructions.OperationAreas != null)
+            { products = products.Where(x => x.ProductOperationAreas.Intersect(searchInstructions.OperationAreas).Any()); }
+
+            return products;
+        }
+    }
+}
