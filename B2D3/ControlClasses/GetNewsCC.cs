ad83555a3fa2aa2bf80726cfd569039380c15577
Xavier <xavier_vinken@hotmail.com>
2017-06-26 10:16:59 +0200

    nieuws get bestaat

---
+++ B2D3/ControlClasses/GetNewsCC.cs
@@ -0,0 +1,18 @@
+﻿using B2D3.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace B2D3.ControlClasses
+{
+    public class GetNewsCC
+    {
+        private static News newsC = new News();
+
+        public List<News> GetNews()
+        {
+            return newsC.Getnews();
+        }
+    }
+}
