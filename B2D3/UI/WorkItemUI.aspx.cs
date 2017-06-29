3ea5a5d6acc3b1e14da9da144bf1ca1f2876df51
Nathan <Nathan@NATHAN-PC>
2017-06-29 19:10:47 +0200

    first update

---
+++ B2D3/UI/WorkItemUI.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using B2D3.Classes.CC;
+
+namespace B2D3.UI
+{
+    public partial class WorkItemUI : System.Web.UI.Page
+    {
+        WorkItemCC wi = new WorkItemCC();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            GridView1.DataSource = wi.FetchTable();
+            GridView1.DataBind();
+        }
+
+    }
+}
--- B2D3/default.aspx.cs
+++ B2D3/default.aspx.cs
@@ -11,7 +11,7 @@ namespace B2D3
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Server.Transfer("~/UI/WorkItemUI.aspx");
         }
     }
 }
