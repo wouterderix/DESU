d804744761655cd16416cd20d16386fe1fab3769
Job Hollands <job.hollands@hotmail.nl>
2017-06-30 11:50:25 +0200

    Contact bijgevoegd

---
+++ B2D3/ControlClasses/CCContactOpnemen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace B2D3.Classes.CC
+{
+    [Author("Job Hollands", "Contact Opnemen")]
+    public class CCContactOpnemen
+    {
+        public bool SaveMail(string name, string email, string subject, string message, User user)
+        {
+            Contact contact = new Contact();
+
+            if (contact.saveMail(name, email, subject, message, user))
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        public DataTable GetSupervisors()
+        {
+            return new Contact().GetSupervisors();   
+        }
+    }
+}
---
+++ B2D3/UI/Contact.aspx.cs
@@ -0,0 +1,88 @@
+﻿using B2D3.Classes.CC;
+using System;
+
+namespace B2D3.Classes.UI
+{
+    [Author("Job Hollands", "Contact Opnemen")]
+    public partial class Contact : System.Web.UI.Page
+    {
+        private static User _user;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGrid();
+            }
+
+            if (User.Identity.IsAuthenticated)
+            {
+                if (!cbAnoniem.Checked)
+                {
+                    //Alle gegevens van user verkrijgen
+
+                    //VIA IDENTITY
+                    //_userID = User.Identity.GetUserId();
+
+                    //VIA IDENTITY
+                    //tbName.Text = User.Identity.GetUserName();
+
+                    //Via 5 lagen-model
+                    //tbEmail.Text = new CCContactOpnemen().GetEmailAdress(_userID);
+                }
+                else
+                {
+                    _user = null;
+                }
+                btSend.Attributes.Remove("onclick");
+            }
+
+            else
+            {
+                _user = null;
+                //Gebruiker vragen of hij of zij wilt inloggen
+                btSend.Attributes.Add("onclick", "return ConfirmOnClick()");
+
+                if (cbAnoniem.Checked)
+                {
+                    btSend.Attributes.Remove("onclick");
+                }
+            }
+        }
+
+        private void BindGrid()
+        {
+            gvGetSupervisors.DataSource = new CCContactOpnemen().GetSupervisors();
+            gvGetSupervisors.DataBind();
+        }
+
+        protected void btSend_Click(object sender, EventArgs e)
+        {
+            CCContactOpnemen co = new CCContactOpnemen();
+
+            if (co.SaveMail(tbName.Text, tbEmail.Text, tbSubject.Text, tbMessage.Text, _user))
+            {
+                ClientScript.RegisterStartupScript(GetType(), "alert", "alert('Mail is succesvol verzonden');", true);
+            }
+            else
+            {
+                ClientScript.RegisterStartupScript(GetType(), "alert", "alert('Mail verzonden is mislukt');", true);
+            }
+        }
+
+        protected void cbAnoniem_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbAnoniem.Checked)
+            {
+                tbName.Text = "Anoniem";
+                tbEmail.Text = "anoniem.anoniem@a.com";
+            }
+
+            else
+            {
+                tbName.Text = "";
+                tbEmail.Text = "";
+            }
+        }
+
+    }
+}
