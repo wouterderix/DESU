84156b0f3c2bd49995ed476fe06f64af0b92b16f
Dennis <Dennis@LAPTOPDENNIS>
2017-06-30 11:11:13 +0200

    .

--- B2D3/ControlClasses/SearchProduct.cs
+++ B2D3/ControlClasses/SearchProduct.cs
@@ -79,25 +79,6 @@ namespace B2D3.ControlClasses
             => ExecuteProductQuerry(productQuerry);
 
         private static DataTable ExecuteProductQuerry(ProductQuerryModel productQuerry)
-        {
-            //DataTable dt = new DataTable();
-            //for (int i = 0; i < numbers.GetLength(1); i++)
-            //{
-            //    dt.Columns.Add("Column" + (i + 1));
-            //}
-
-            //for (var i = 0; i < numbers.GetLength(0); ++i)
-            //{
-            //    DataRow row = dt.NewRow();
-            //    for (var j = 0; j < numbers.GetLength(1); ++j)
-            //    {
-            //        row[j] = numbers[i, j];
-            //    }
-            //    dt.Rows.Add(row);
-            //}
-            //return dt;
-            //return Webserver.Instance.SearchProducts(productQuerry);
-            throw new NotImplementedException("Finish me");
-        }
+        { return DatatableBuilder.BuildDatatable(Webserver.Instance.SearchProducts(productQuerry)); }
     }
 }
